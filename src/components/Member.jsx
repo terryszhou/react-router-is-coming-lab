9c1adc977d86cdff01a4410fb0010a55c0751d0b
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-28 17:29:52 -0500

    Added ASOIAF links to member component

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -2,7 +2,11 @@ export const Member = (props) => {
     return (
         <div className="page">
             <div className="header">
-                <h2>{props.name}</h2>
+                <a href={`https://awoiaf.westeros.org/index.php/${props.wikiSuffix}`}>
+                    <h2>
+                        {props.name}
+                    </h2>
+                </a>
             </div>
             <div className="desc">
                 <p>{props.description}</p>
